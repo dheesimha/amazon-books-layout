339fdda6c3b4a546ba9b33e1d15de26ce90239a4
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2021-07-01 12:59:22 +0530

    Map through a sample list

--- src/index.js
+++ src/index.js
@@ -28,38 +28,29 @@ import "./index.css";
 //   return <p>This is a message</p>;
 // };
 
-const firstBook = {
-  img: "https://m.media-amazon.com/images/I/71aFt4+OTOL._AC_UY327_FMwebp_QL65_.jpg",
-  author: "Paulo Coleho",
-  title: "The Alchemist",
-};
+const books = [
+  {
+    img: "https://m.media-amazon.com/images/I/71aFt4+OTOL._AC_UY327_FMwebp_QL65_.jpg",
+    author: "Paulo Coleho",
+    title: "The Alchemist",
+  },
 
-const secondBook = {
-  img: "https://m.media-amazon.com/images/I/710jnzKlDTL._AC_UY327_FMwebp_QL65_.jpg",
-  author: "Jeff Keller",
-  title: "Attitude Is Everything ",
-};
+  {
+    img: "https://m.media-amazon.com/images/I/710jnzKlDTL._AC_UY327_FMwebp_QL65_.jpg",
+    author: "Jeff Keller",
+    title: "Attitude Is Everything ",
+  },
+];
 
+const names = ["Dhee", "Zoko", "Sahas"];
+const newName = names.map((name) => {
+  return <h1>{name}</h1>;
+  console.log(name);
+});
+
+console.log(newName);
 function BookList() {
-  return (
-    <section className="bookList">
-      <Book
-        img={firstBook.img}
-        author={firstBook.author}
-        title={firstBook.title}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quam
-          ab animi porro voluptas a mollitia dolorem eos at laboriosam.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        author={secondBook.author}
-        title={secondBook.title}
-      />
-    </section>
-  );
+  return <section className="bookList">{names}</section>;
 }
 
 // const title = "The Alchemist";
@@ -68,14 +59,13 @@ function BookList() {
 //   "https://m.media-amazon.com/images/I/71aFt4+OTOL._AC_UY327_FMwebp_QL65_.jpg";
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author } = props;
   console.log(props);
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
