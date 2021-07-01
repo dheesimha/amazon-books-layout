0dcfda9c763412caf645875b5db9a2691a17e8bc
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2021-07-01 11:07:15 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import react from "react";
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+// function Greeting() {
+//   return <h4>Hello world</h4>;
+// }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// const Greeting = () => {
+// <ReactFragment>
+//   return React.createElement("h1", {}, "Hello world");
+// </ReactFragment>;
+// };
+
+// function BookList() {
+//   return (
+//     <section>
+//       {/* <h2>Dheemanth</h2>
+//       <Person />
+//       <Message /> */}
+
+//     </section>
+//   );
+// }
+
+// const Person = () => <h2>Dheemanth</h2>;
+// const Message = () => {
+//   return <p>This is a message</p>;
+// };
+
+function BookList() {
+  return (
+    <section className="bookList">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      {" "}
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/51MtWwKWcuL._AC_SX360_SY240_QL70_.jpg"
+    alt=""
+  />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title = () => (
+  <h1>
+    World's Greatest Entrepreneurs: Biographies of Inspirational Personalities
+    For Kids
+  </h1>
+);
+
+const Author = () => {
+  return <p>Wonder House Books</p>;
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
